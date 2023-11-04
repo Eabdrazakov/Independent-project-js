2d710095b68a138a363022f9ab2c9c2f21ea7c46
Padma Patil <padma@email.com>
2023-11-04 08:27:36 -0700

    Add function hideResult()

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//Business Logic
+function hideResults() {
+    document.getElementById("java").setAttribute("class", hidden);
+    document.getElementById("ios").setAttribute("class", hidden);
+    document.getElementById("python").setAttribute("class", hidden);
+    document.getElementById("c#").setAttribute("class", hidden);
+    document.getElementById("ruby").setAttribute("class", hidden);
+}
