bb383aed3f484dc49c60ed0f010feb936f3d12db
Padma Patil <padma@email.com>
2023-11-04 08:35:47 -0700

    Add function

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,15 @@ function hideResults() {
     document.getElementById("c#").setAttribute("class", hidden);
     document.getElementById("ruby").setAttribute("class", hidden);
 }
+
+
+function processFormValues() {
+    hideResults();
+    const wed = document.querySelector("input#web").value;
+    const mob = document.querySelector("input#mob").value;
+    const ana = document.querySelector("input#ana").value;
+    const dev = document.querySelector("input#dev").value;
+    const gen = document.querySelector("input#gen").value;
+
+
+}
