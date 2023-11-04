52e7ac90ea676337ad8ecba2cc3288102d06101b
Padma Patil <padma@email.com>
2023-11-04 08:44:05 -0700

    Add if/else

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,23 @@ function hideResults() {
 
 function processFormValues() {
     hideResults();
-    const wed = document.querySelector("input#web").value;
+    const web = document.querySelector("input#web").value;
     const mob = document.querySelector("input#mob").value;
     const ana = document.querySelector("input#ana").value;
     const dev = document.querySelector("input#dev").value;
     const gen = document.querySelector("input#gen").value;
 
-
+    if (web || mob || ana || dev || gen) {
+        if (web) {
+            document.getElementById("java").removeAttribute("class");
+        } else if (mob) {
+            document.getElementById("ios").removeAttribute("class");
+        } else if (ana) {
+            document.getElementById("python").removeAttribute("class");
+        } else if (dev) {
+            document.getElementById("c#").removeAttribute("class");
+        } else if (gen) {
+            document.getElementById("ruby").removeAttribute("class");
+        }
+    }
 }
